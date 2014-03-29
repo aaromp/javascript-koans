8662a647fddcae677eaa0c24bafb1ac487db334a
Aaron Ward <aaromp@gmail.com>
2014-03-28 22:36:49 -0700

    Add changes made after adding repository to github

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(3);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -40,8 +40,11 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+	  var hasMushrooms = _(products).all(function(x) { return x.ingredients !== "mushrooms"});
+	  
+	  productsICanEat = _(hasMushrooms).filter(function(x) { return x.containsNuts !=== true })
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(3);
   });
 
   /*********************************************************************************/
